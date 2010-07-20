af0425cb8b46b73f1e6f6f112d164ed6c0f639e2
Robert Norris <rob@eatenbyagrue.org>
2010-07-21 07:36:30 +1000

    event context class

--- lib/Google/Wave/Robot/Event/Context.pm
+++ lib/Google/Wave/Robot/Event/Context.pm
@@ -3,6 +3,15 @@ package Google::Wave::Robot::Event::Context;
 use warnings;
 use strict;
 
+use constant {
+    ROOT     => "ROOT",
+    PARENT   => "PARENT",
+    SIBLINGS => "SIBLINGS",
+    CHILDREN => "CHILDREN",
+    SELF     => "SELF",
+    ALL      => "ALL",
+};
+
 1;
 
 __END__

a7ef7846fbf5b185309fcdde6b92e05be213470b
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 19:37:28 +1000

    basic structure for robot class

--- lib/Google/Wave/Robot.pm
+++ lib/Google/Wave/Robot.pm
@@ -1,7 +1,35 @@
 package Google::Wave::Robot;
-# ABSTRACT: turns baubles into trinkets
+# ABSTRACT: Wave Robot client library for Perl
 
-use warnings;
-use strict;
+use 5.010;
+
+our $VERSION = '0.01';
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+method verification_token_info () {
+}
+
+method setup_oauth () {
+}
+
+method register_handler () {
+}
+
+method register_profile_handler () {
+}
+
+method capabilities_xml () {
+}
+
+method process_events () {
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__

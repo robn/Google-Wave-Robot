0cb61df002ef035425874092e994d495f48ab55c
Robert Norris <rob@eatenbyagrue.org>
2010-07-21 07:26:58 +1000

    operationerror

--- lib/Google/Wave/Robot/Event/OperationError.pm
+++ lib/Google/Wave/Robot/Event/OperationError.pm
@@ -1,7 +1,36 @@
 package Google::Wave::Robot::Event::OperationError;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "OPERATION_ERROR";
+
+has "operation_id" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+has "message" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+method BUILDARGS ( ClassName $class: HashRef :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    return {
+        operation_id => $json->{properties}->{operationId},
+        message      => $json->{properties}->{message},
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

bbd7b388afa4156d296690f65fae49d53d4efa79
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 22:10:43 +1000

    blipsubmitted

--- lib/Google/Wave/Robot/Event/BlipSubmitted.pm
+++ lib/Google/Wave/Robot/Event/BlipSubmitted.pm
@@ -1,7 +1,20 @@
 package Google::Wave::Robot::Event::BlipSubmitted;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use MooseX::Types::JSON qw(JSON);
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "BLIP_SUBMITTED";
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

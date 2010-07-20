2abd8169ae57d94fcc86b771de14e125a8d20f85
Robert Norris <rob@eatenbyagrue.org>
2010-07-21 07:24:08 +1000

    gadgetstatechanged

--- lib/Google/Wave/Robot/Event/GadgetStateChanged.pm
+++ lib/Google/Wave/Robot/Event/GadgetStateChanged.pm
@@ -1,7 +1,36 @@
 package Google::Wave::Robot::Event::GadgetStateChanged;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "GADGET_STATE_CHANGED";
+
+has "index" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+has "old_state" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+method BUILDARGS ( ClassName $class: HashRef :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    return {
+        index     => $json->{properties}->{index},
+        old_state => $json->{properties}->{oldState},
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

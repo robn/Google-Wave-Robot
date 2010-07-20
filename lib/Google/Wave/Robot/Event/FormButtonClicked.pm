d2e8dcbefb6d8a93c5e85cacddc2bf0504ffd8d1
Robert Norris <rob@eatenbyagrue.org>
2010-07-21 07:19:36 +1000

    formbuttonclicked

--- lib/Google/Wave/Robot/Event/FormButtonClicked.pm
+++ lib/Google/Wave/Robot/Event/FormButtonClicked.pm
@@ -1,7 +1,31 @@
 package Google::Wave::Robot::Event::FormButtonClicked;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use MooseX::Types::JSON qw(JSON);
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "FORM_BUTTON_CLICKED";
+
+has "button_name" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+method BUILDARGS ( ClassName $class: JSON :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    return {
+        button_name => $json->{properties}->{buttonName};
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

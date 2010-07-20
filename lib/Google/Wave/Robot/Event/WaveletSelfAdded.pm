5aca9ddc3c3f6ecc7b00010b4bbca90e087ca6a9
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 22:03:42 +1000

    selfadded/selfremoved events

--- lib/Google/Wave/Robot/Event/WaveletSelfAdded.pm
+++ lib/Google/Wave/Robot/Event/WaveletSelfAdded.pm
@@ -1,7 +1,20 @@
 package Google::Wave::Robot::Event::WaveletSelfAdded;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use MooseX::Types::JSON qw(JSON);
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "WAVELET_SELF_ADDED";
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

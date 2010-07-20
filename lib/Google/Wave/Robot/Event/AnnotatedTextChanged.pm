37a2089f523f125ab4d8de4a06d45e4631af67e6
Robert Norris <rob@eatenbyagrue.org>
2010-07-21 07:25:49 +1000

    annotatedtextchanged

--- lib/Google/Wave/Robot/Event/AnnotatedTextChanged.pm
+++ lib/Google/Wave/Robot/Event/AnnotatedTextChanged.pm
@@ -1,7 +1,36 @@
 package Google::Wave::Robot::Event::AnnotatedTextChanged;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "ANNOTATED_TEXT_CHAGED";
+
+has "name" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+has "value" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+method BUILDARGS ( ClassName $class: HashRef :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    return {
+        name  => $json->{properties}->{name},
+        value => $json->{properties}->{value},
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

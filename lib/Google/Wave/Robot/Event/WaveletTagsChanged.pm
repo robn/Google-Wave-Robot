e807ef1ac225e390c3ce3696d41fc1c10d4f4c5d
Robert Norris <rob@eatenbyagrue.org>
2010-07-21 07:33:39 +1000

    wavelettagschanged

--- lib/Google/Wave/Robot/Event/WaveletTagsChanged.pm
+++ lib/Google/Wave/Robot/Event/WaveletTagsChanged.pm
@@ -1,7 +1,19 @@
 package Google::Wave::Robot::Event::WaveletTagsChanged;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "WAVELET_TAGS_CHANGED";
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

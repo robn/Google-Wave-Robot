2e7f9c4e078c02d9966003757ed2759615572632
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 21:17:36 +1000

    event baseclass

--- lib/Google/Wave/Robot/Event.pm
+++ lib/Google/Wave/Robot/Event.pm
@@ -1,7 +1,76 @@
 package Google::Wave::Robot::Event;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+use Moose::Util::TypeConstraints;
+use MooseX::Types::JSON qw(JSON);
+
+BEGIN {
+    class_type 'Google::Wave::Robot::Wavelet';
+}
+
+has "json" => {
+    is  => "ro",
+    isa => "Str",
+};
+
+has "type" => {
+    is  => "ro",
+    isa => "Str",
+};
+
+has "modified_by" => {
+    is  => "ro",
+    isa => "Str",
+};
+
+has "timestamp" => {
+    is  => "ro",
+    isa => "Int",
+};
+
+has "proxying_for" => {
+    is  => "ro",
+    isa => "Str",
+};
+
+has "properties" => {
+    is  => "ro",
+    isa => "HashRef",
+};
+
+has "blip_id" => {
+    is  => "ro",
+    isa => "Str",
+};
+
+has "blip" => {
+    is  => "ro",
+    isa => "Google::Wave::Robot::Blip",
+};
+
+method BUILDARGS ( ClassName $class: JSON :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    my $args = {
+        json         => $json,
+        type         => $json->{type},
+        modified_by  => $json->{modifiedBy},
+        timestamp    => $json->{timestamp} // 0,
+        proxying_for => $json->{proxyingFor},
+    };
+
+    $args->{properties} = $json->{properties} // {},
+
+    $args->{blip_id} = $args->{properties}->{blipId} if exists $args->{properties}->{blip_id};
+    $args->{blip} = $wavelet->blip($args->{blip_id}) if exists $args->{blip_id};
+
+    return $args;
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

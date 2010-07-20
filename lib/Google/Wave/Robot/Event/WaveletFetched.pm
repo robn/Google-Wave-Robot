d2e0ee65c53a8f67649200e0834baf09593afad2
Robert Norris <rob@eatenbyagrue.org>
2010-07-21 07:32:36 +1000

    waveletfetched

--- lib/Google/Wave/Robot/Event/WaveletFetched.pm
+++ lib/Google/Wave/Robot/Event/WaveletFetched.pm
@@ -1,7 +1,30 @@
 package Google::Wave::Robot::Event::WaveletFetched;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "WAVELET_FETCHED";
+
+has "message" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+method BUILDARGS ( ClassName $class: HashRef :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    return {
+        message => $json->{properties}->{message},
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

e0bcfd4ce0572e5681742446ad4b622f3d8b2906
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 22:09:27 +1000

    blipcontributorschanged

--- lib/Google/Wave/Robot/Event/BlipContributorsChanged.pm
+++ lib/Google/Wave/Robot/Event/BlipContributorsChanged.pm
@@ -1,7 +1,37 @@
 package Google::Wave::Robot::Event::BlipContributorsChanged;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use MooseX::Types::JSON qw(JSON);
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "BLIP_CONTRIBUTORS_CHANGED";
+
+has "contributors_added" => (
+    is  => "ro",
+    isa => "ArrayRef[Str]",
+);
+
+has "contributors_removed" => (
+    is  => "ro",
+    isa => "ArrayRef[Str]",
+);
+
+method BUILDARGS ( ClassName $class: JSON :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    return {
+        contributors_added   => $json->{properties}->{contributorsAdded},
+        contributors_removed => $json->{properties}->{contributorsRemoved},
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

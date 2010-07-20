a627616610e509b488779d650d0e254dae3f7c2e
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 21:30:02 +1000

    an event subclass

--- lib/Google/Wave/Robot/Event/WaveletBlipCreated.pm
+++ lib/Google/Wave/Robot/Event/WaveletBlipCreated.pm
@@ -1,7 +1,42 @@
-package Google::Wave::Robot::Event::WaveletBlipCreated;
+package Google::Wave::Event::WaveletBlipCreated;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+use Moose::Util::TypeConstraints;
+use MooseX::Types::JSON qw(JSON);
+
+BEGIN {
+    class_type 'Google::Wave::Robot::Wavelet';
+}
+
+extends ("Google::Wave::Event");
+
+use constant name => "WAVELET_BLIP_CREATED";
+
+has "new_blip_id" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+has "new_blip" => (
+    is  => "ro",
+    isa => "Google::Wave::Robot::Blip",
+);
+
+method BUILDARGS ( ClassName $class: JSON :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    my $args;
+
+    $args->{new_blip_id} = $json->{properties}->{newBlipId};
+    $args->{new_blip}    = $wavelet->blip($args->{new_blip_id});
+
+    return $args;
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

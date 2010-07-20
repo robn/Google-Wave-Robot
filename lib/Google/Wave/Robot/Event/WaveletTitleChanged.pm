4ebc6565d9c50c6f381027f77d3b7ed33b542bcb
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 22:07:44 +1000

    wavelettitlechanged

--- lib/Google/Wave/Robot/Event/WaveletTitleChanged.pm
+++ lib/Google/Wave/Robot/Event/WaveletTitleChanged.pm
@@ -1,7 +1,31 @@
 package Google::Wave::Robot::Event::WaveletTitleChanged;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use MooseX::Types::JSON qw(JSON);
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "WAVELET_TITLE_CHANGED";
+
+has "title" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+method BUILDARGS ( ClassName $class: JSON :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    return {
+        title => $json->{properties}->{title},
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

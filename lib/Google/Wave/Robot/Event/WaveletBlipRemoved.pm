45d0b76993a2b89a8460030b53136eb91a665856
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 21:56:47 +1000

    waveletblipremoved event

--- lib/Google/Wave/Robot/Event/WaveletBlipRemoved.pm
+++ lib/Google/Wave/Robot/Event/WaveletBlipRemoved.pm
@@ -1,7 +1,31 @@
 package Google::Wave::Robot::Event::WaveletBlipRemoved;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use MooseX::Types::JSON qw(JSON);
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "WAVELET_BLIP_REMOVED";
+
+has "removed_blip_id" => (
+    is  => "ro",
+    isa => "Str",
+);
+
+method BUILDARGS ( ClassName $class: JSON :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    return {
+        removed_blip_id => $json->{properties}->{removedBlipId},
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

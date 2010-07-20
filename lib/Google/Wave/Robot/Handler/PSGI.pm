8c07f4b04a387f0a33e37e44b3c11575e326c369
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 20:32:17 +1000

    start of a general psgi robot handler

---
+++ lib/Google/Wave/Robot/Handler/PSGI.pm
@@ -0,0 +1,60 @@
+package Google::Wave::Robot::Handler::PSGI;
+
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+use Moose::Util::TypeConstraints;
+
+BEGIN {
+    class_type 'Google::Wave::Robot';
+}
+
+method run ( ClassName $class: Google::Wave::Robot $robot, HashRef $env ) {
+    given ($env->{REQUEST_URI}) {
+        when (m{/_wave/capabilities.xml$}) {
+            return $class->capabilities_handler($robot);
+        }
+        when (m{/_wave/robot/profile$}) {
+            return $class->profiles_handler($robot);
+        }
+        when (m{/_wave/robot/jsonrpc$}) {
+            return $class->rpc_handler($robot);
+        }
+        when (m{/_wave/verify_token$}) {
+            return $class->verify_token_handler($robot);
+        }
+    }
+
+    return $class->unknown_handler;
+}
+
+method capabilities_handler ( ClassName $class: Google::Wave::Robot $robot ) {
+}
+
+method profiles_handler ( ClassName $class: Google::Wave::Robot $robot ) {
+}
+
+method rpc_handler ( ClassName $class: Google::Wave::Robot $robot ) {
+}
+
+method verify_token_handler ( ClassName $class: Google::Wave::Robot $robot ) {
+}
+
+method unknown_handler ( ClassName $class: ) {
+    my $status = 404;
+    my $headers = [
+        'Content-Type' => 'text/plain',
+    ];
+    my $body = [ 'not found' ];
+
+    return [ $status, $headers, $body ];
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__

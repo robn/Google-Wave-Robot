face0d3d047ebaf9bbed125cc44b9a4c19a1330e
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 22:01:22 +1000

    waveletparticipantschanged event

--- lib/Google/Wave/Robot/Event/WaveletParticipantsChanged.pm
+++ lib/Google/Wave/Robot/Event/WaveletParticipantsChanged.pm
@@ -1,7 +1,37 @@
 package Google::Wave::Robot::Event::WaveletParticipantsChanged;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+use MooseX::Types::JSON qw(JSON);
+use Google::Wave::Robot::Types;
+
+extends ("Google::Wave::Robot::Event");
+
+use constant name => "WAVELET_PARTICIPANTS_CHANGED";
+
+has "participants_added" => (
+    is  => "ro",
+    isa => "ArrayRef[Str]",
+);
+
+has "participants_removed" => (
+    is  => "ro",
+    isa => "ArrayRef[Str]",
+);
+
+method BUILDARGS ( ClassName $class: JSON :$json, Google::Wave::Robot::Wavelet :$wavelet ) {
+    return {
+        participants_added   => $json->{properties}->{particpantsAdded},
+        participants_removed => $json->{properties}->{particpantsRemoved},
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

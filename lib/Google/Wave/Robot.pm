9c8ccb60145e1d704c5ee4a182c3e2ce3927e5e3
Robert Norris <rob@eatenbyagrue.org>
2010-07-21 21:32:23 +1000

    initial handler registration (no contexts or filters yet)

--- lib/Google/Wave/Robot.pm
+++ lib/Google/Wave/Robot.pm
@@ -10,13 +10,51 @@ use namespace::autoclean;
 use Moose;
 use MooseX::Method::Signatures;
 
+use Google::Wave::Robot::Operation;
+use Google::Wave::Robot::Event;
+
+has "_handlers" => (
+    is      => "rw",
+    isa     => "HashRef",
+    default => sub { {} },
+);
+
+has "capabilities_hash" => (
+    is      => "ro",
+    isa     => "Int",
+    writer  => "_set_capabilities_hash",
+    default => 0,
+);
+
 method verification_token_info () {
 }
 
 method setup_oauth () {
 }
 
+my $hash_func = sub {
+    require B;
+    return hex(B::hash(shift));
+};
+
+# XXX include context and filter
 method register_handler ( Str $event_class, CodeRef $handler ) {
+    my $type = $event_class->type;
+
+    $self->_handlers->{$type} = {
+        event_class => $event_class,
+        handler     => $handler,
+        # XXX context
+        # XXX filter
+    };
+
+    $self->_set_capabilities_hash((
+        $self->capabilities_hash * 13 +
+        $hash_func->(Google::Wave::Robot::Operation::PROTOCOL_VERSION) + 
+        $hash_func->($type)
+        # XXX $hash_func->($context) +
+        # XXX $hash_func->($filter)
+    ) & 0xfffffff);
 }
 
 method register_profile_handler () {

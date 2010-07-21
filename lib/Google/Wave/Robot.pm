bee48f07c4d0e51eecec5f75d88b6809d64af64e
Robert Norris <rob@eatenbyagrue.org>
2010-07-21 22:16:10 +1000

    json event dispatcher and responder

--- lib/Google/Wave/Robot.pm
+++ lib/Google/Wave/Robot.pm
@@ -9,9 +9,16 @@ use namespace::autoclean;
 
 use Moose;
 use MooseX::Method::Signatures;
+use MooseX::Types::JSON qw(JSON);
 
-use Google::Wave::Robot::Operation;
+use Google::Wave::Robot::Types;
+
 use Google::Wave::Robot::Event;
+use Google::Wave::Robot::Operation;
+use Google::Wave::Robot::Operation::Queue;
+use Google::Wave::Robot::Wavelet;
+
+use JSON qw(encode_json decode_json);
 
 has "_handlers" => (
     is      => "rw",
@@ -63,7 +70,24 @@ method register_profile_handler () {
 method capabilities_xml () {
 }
 
-method process_events () {
+method process_events ( JSON $json ) {
+    my $parsed = decode_json($json);
+
+    my $pending_ops = Google::Wave::Robot::Operation::Queue->new;
+    my $event_wavelet = Google::Wave::Robot::Wavelet->new_from_json(
+        json            => $parsed, 
+        operation_queue => $pending_ops,
+    );
+
+    for my $event_data (@{$parsed->{events}}) {
+        if (my $handler = $self->_handlers->{$event_data->{type}}) {
+            my $event = $handler->{event_class}->new($event_data, $event_wavelet);
+            $handler->{handler}->($event, $event_wavelet);
+        }
+    }
+
+    $pending_ops->capabilities_hash($self->capabilities_hash);
+    return encode_json($pending_ops->serialize);
 }
 
 __PACKAGE__->meta->make_immutable;

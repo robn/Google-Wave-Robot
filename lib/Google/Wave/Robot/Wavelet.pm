b3e43e0776e05b6f9c3f096110e65f46723625f9
Robert Norris <rob@eatenbyagrue.org>
2010-07-23 15:30:23 +1000

    too many quotes

--- lib/Google/Wave/Robot/Wavelet.pm
+++ lib/Google/Wave/Robot/Wavelet.pm
@@ -10,69 +10,69 @@ use MooseX::Types::Moose qw(Str HashRef ArrayRef Object);
 use Google::Wave::Robot::Types qw(Blip OperationQueue);
 
 has "wavelet_id" => (
-    "is"  => "ro",
-    "isa" => Str,
+    is  => "ro",
+    isa => Str,
 );
 
 has "wave_id" => (
-    "is"  => "ro",
-    "isa" => Str,
+    is  => "ro",
+    isa => Str,
 );
 
 has "creator" => (
-    "is"  => "ro",
-    "isa" => Str,
+    is  => "ro",
+    isa => Str,
 );
 
 has "creation_time" => (
-    "is"  => "ro",
-    "isa" => Str,
+    is  => "ro",
+    isa => Str,
 );
 
 has "data_documents" => (
-    "is"  => "ro",
-    "isa" => HashRef[Str],    # XXX key/value pairs, needs some handlers that hook DATADOC_SET
+    is  => "ro",
+    isa => HashRef[Str],    # XXX key/value pairs, needs some handlers that hook DATADOC_SET
 );
 
 has "last_modified_time" => (
-    "is"  => "ro",
-    "isa" => Str,
+    is  => "ro",
+    isa => Str,
 );
 
 has "participants" => (
-    "is"  => "ro",
-    "isa" => ArrayRef[Str],  # XXX list of addresses, needs handlers that hook ADD_PARTICIPANT
+    is  => "ro",
+    isa => ArrayRef[Str],  # XXX list of addresses, needs handlers that hook ADD_PARTICIPANT
                              # XXX seperate objects with roles included? +profiles when that api hits
 );
 
 has "root_thread" => (
-    "is"  => "ro",
-    "isa" => Object,  # XXX BlipThread
+    is  => "ro",
+    isa => Object,  # XXX BlipThread
 );
 
 has "tags" => (
-    "is"  => "ro",
-    "isa" => ArrayRef[Str], # list of tags, needs handlers that hook MODIFY_TAG
+    is  => "ro",
+    isa => ArrayRef[Str], # list of tags, needs handlers that hook MODIFY_TAG
 );
 
 has "title" => (
-    "is"  => "rw",
-    "isa" => Str,  # XXX hook setter to call set_title and adjust the content
+    is  => "rw",
+    isa => Str,  # XXX hook setter to call set_title and adjust the content
 );
 
 has "robot_address" => (
-    "is"  => "ro",
-    "isa" => Str,  # XXX can't be set if already set
+    is  => "ro",
+    isa => Str,  # XXX can't be set if already set
 );
 
 has "root_blip" => (
-    "is"  => "ro",
-    "isa" => Blip,
+    is  => "ro",
+    isa => Blip,
 );
 
 has "operation_queue" => (
-    "is"  => "ro",
-    "isa" => OperationQueue,
+    is  => "ro",
+    isa => OperationQueue,
 );
 
 method new_from_json ( HashRef :$json, OperationQueue :$operation_queue ) {

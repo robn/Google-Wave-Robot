9e3ef7622e625608317f40403d8c5e1e472b5ef2
Robert Norris <rob@eatenbyagrue.org>
2010-07-24 19:15:13 +1000

    start of the blip class

--- lib/Google/Wave/Robot/Blip.pm
+++ lib/Google/Wave/Robot/Blip.pm
@@ -1,7 +1,107 @@
 package Google::Wave::Robot::Blip;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+use MooseX::Types::Moose qw(Str HashRef ArrayRef);
+use Google::Wave::Robot::Types qw(OperationQueue Blip);
+
+has blip_id => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has wavelet_id => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has wave_id => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has parent_blip_id => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has child_blip_ids => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has creator => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has contributors => (
+    is       => "ro",
+    isa      => ArrayRef[Str],
+    required => 1,
+);
+
+has last_modified_time => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has version => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has text => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has annotations => (
+    is       => "ro",
+    isa      => ArrayRef,   # XXX ArrayRef[Annotation]
+);
+
+has elements => (
+    is       => "ro",
+    isa      => ArrayRef,   # XXX ArrayRef[Elements]
+);
+
+method BUILDARGS ( ClassName $class: HashRef :$json, OperationQueue :$operation_queue?, ArrayRef[Blip] :$other_blips )
+{
+    my $args = {};
+
+    $args->{operation_queue} = $operation_queue || Google::Wave::Robot::Operation::Queue->new;
+
+    $args->{blip_id}            = $json->{blipId};
+    $args->{wavelet_id}         = $json->{waveletId};
+    $args->{wave_id}            = $json->{waveId};
+    $args->{parent_blip_id}     = $json->{parentBlipId};
+    $args->{child_blip_ids}     = $json->{childBlipIds};
+    $args->{creator}            = $json->{creator};
+    $args->{contributors}       = $json->{contributors};
+    $args->{last_modified_time} = $json->{lastModifiedTime};
+    $args->{version}            = $json->{version};
+    $args->{text}               = $json->{content};
+
+    # XXX annotations, elements
+    
+    return $args;
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

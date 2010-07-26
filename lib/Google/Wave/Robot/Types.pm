32792e5f157ca84f2ae09f9f5db4cecdab750b41
Robert Norris <rob@eatenbyagrue.org>
2010-07-27 09:19:02 +1000

    oops

--- lib/Google/Wave/Robot/Types.pm
+++ lib/Google/Wave/Robot/Types.pm
@@ -5,7 +5,7 @@ use 5.010;
 use namespace::autoclean;
 
 use MooseX::Types -declare => [qw(
-    Robot Wavelet Blip BlipSet Operation OperationQueue Participant ParticipantSet
+    Robot Wavelet Blip BlipSet Operation OperationQueue Participant ParticipantSet TagSet
 )];
 
 class_type Robot,          { class => 'Google::Wave::Robot' };

044c5d57f73f6bb5fb2c32dd8806dfaf28c49977
Robert Norris <rob@eatenbyagrue.org>
2010-07-26 22:22:53 +1000

    fix participantset class

--- lib/Google/Wave/Robot/Types.pm
+++ lib/Google/Wave/Robot/Types.pm
@@ -15,7 +15,7 @@ class_type BlipSet,        { class => 'Google::Wave::Robot::Blip::Set' };
 class_type Operation,      { class => 'Google::Wave::Robot::Operation' };
 class_type OperationQueue, { class => 'Google::Wave::Robot::Operation::Queue' };
 class_type Participant,    { class => 'Google::Wave::Robot::Participant' };
-class_type ParticipantSet, { class => 'Google::Wave::Robot::ParticipantSet' };
+class_type ParticipantSet, { class => 'Google::Wave::Robot::Participant::Set' };
 
 1;
 

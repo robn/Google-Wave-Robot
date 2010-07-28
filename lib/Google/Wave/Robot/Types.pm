a13332658ff235cc4bcd5174dadadb82dbbc487b
Robert Norris <rob@eatenbyagrue.org>
2010-07-28 16:37:20 +1000

    string->participant coercion

--- lib/Google/Wave/Robot/Types.pm
+++ lib/Google/Wave/Robot/Types.pm
@@ -8,6 +8,10 @@ use MooseX::Types -declare => [qw(
     Robot Wavelet Blip BlipSet Operation OperationQueue Participant ParticipantSet
 )];
 
+use MooseX::Types::Moose qw(Str);
+
+use Google::Wave::Robot::Participant;
+
 class_type Robot,          { class => 'Google::Wave::Robot' };
 class_type Wavelet,        { class => 'Google::Wave::Robot::Wavelet' };
 class_type Blip,           { class => 'Google::Wave::Robot::Blip' };
@@ -15,6 +19,10 @@ class_type BlipSet,        { class => 'Google::Wave::Robot::Blip::Set' };
 class_type Operation,      { class => 'Google::Wave::Robot::Operation' };
 class_type OperationQueue, { class => 'Google::Wave::Robot::Operation::Queue' };
 class_type Participant,    { class => 'Google::Wave::Robot::Participant' };
+
+coerce Participant,
+    from Str,
+        via { Google::Wave::Robot::Participant->new(id => $_) };
 
 1;
 

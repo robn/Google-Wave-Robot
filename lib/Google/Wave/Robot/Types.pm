f0433a1a958c436243a1795ed47d9120bd75cdf3
Robert Norris <rob@eatenbyagrue.org>
2010-07-28 17:14:51 +1000

    participantset is long gone

--- lib/Google/Wave/Robot/Types.pm
+++ lib/Google/Wave/Robot/Types.pm
@@ -5,7 +5,7 @@ use 5.010;
 use namespace::autoclean;
 
 use MooseX::Types -declare => [qw(
-    Robot Wavelet Blip BlipSet Operation OperationQueue Participant ParticipantSet
+    Robot Wavelet Blip BlipSet Operation OperationQueue Participant
 )];
 
 use MooseX::Types::Moose qw(Str);

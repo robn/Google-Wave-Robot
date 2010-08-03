27af49444bdeaf8bdd1ceed9eb316a7891cbbf55
Robert Norris <rob@eatenbyagrue.org>
2010-08-04 08:33:06 +1000

    attachment element

--- lib/Google/Wave/Robot/Element/Attachment.pm
+++ lib/Google/Wave/Robot/Element/Attachment.pm
@@ -1,7 +1,54 @@
 package Google::Wave::Robot::Element::Attachment;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+use MooseX::Types::Moose qw(Str);
+
+extends ("Google::Wave::Robot::Element");
+
+__PACKAGE__->register_element_class("ATTACHMENT", attributes => [qw(attachmentId attachmentUrl mimeType caption)]);
+
+has attachment_id => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has attachment_url => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has mime_type => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+# XXX required?
+has caption => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+=pod
+# XXX python cl lists this. what is this?
+has data => (
+    is      => "ro",
+    isa     => Str,
+    default => '',
+);
+=cut
+
+
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

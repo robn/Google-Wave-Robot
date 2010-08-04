2139777369c300da45fd2b12672525509dc629fc
Robert Norris <rob@eatenbyagrue.org>
2010-08-04 16:53:57 +1000

    gadget element class

--- lib/Google/Wave/Robot/Element/Gadget.pm
+++ lib/Google/Wave/Robot/Element/Gadget.pm
@@ -1,7 +1,24 @@
 package Google::Wave::Robot::Element::Gadget;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+use MooseX::Types::Moose qw(Str);
+
+extends ("Google::Wave::Robot::Element");
+
+__PACKAGE__->register_element_class("GADGET", attributes => [qw(url)]);
+
+has url => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

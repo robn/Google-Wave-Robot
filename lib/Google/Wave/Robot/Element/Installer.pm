1a44e8ce6aee233de77256f3d56ef41bb473ae11
Robert Norris <rob@eatenbyagrue.org>
2010-08-04 16:38:46 +1000

    installer element

--- lib/Google/Wave/Robot/Element/Installer.pm
+++ lib/Google/Wave/Robot/Element/Installer.pm
@@ -1,7 +1,24 @@
-package Google::Wave::Robot::Element::Installer;
+package Google::Wave::Robot::Element::Installer
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+use MooseX::Types::Moose qw(Str);
+
+extends ("Google::Wave::Robot::Element");
+
+__PACKAGE__->register_element_class("INSTALLER", attributes => [qw(manifest)]);
+
+has manifest => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 

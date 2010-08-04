5e8c66ffd6db5fef7e94fabeafb1a7fd778dceb7
Robert Norris <rob@eatenbyagrue.org>
2010-08-04 16:51:04 +1000

    image class

--- lib/Google/Wave/Robot/Element/Image.pm
+++ lib/Google/Wave/Robot/Element/Image.pm
@@ -1,7 +1,48 @@
 package Google::Wave::Robot::Element::Image;
 
-use warnings;
-use strict;
+use 5.010;
+
+use namespace::autoclean;
+
+use Moose;
+use MooseX::Method::Signatures;
+use MooseX::Types::Moose qw(Str Int);
+
+extends ("Google::Wave::Robot::Element");
+
+__PACKAGE__->register_element_class("IMAGE", attributes => [qw(url width height attachmentId caption)]);
+
+has url => (
+    is       => "ro",
+    isa      => Str,
+    required => 1,
+);
+
+has width => (
+    is      => "ro",
+    isa     => Int,
+    default => 0,
+);
+
+has height => (
+    is      => "ro",
+    isa     => Int,
+    default => 0,
+);
+
+has attachment_id => (
+    is      => "ro",
+    isa     => Str,
+    default => '',
+);
+
+has caption => (
+    is      => "ro",
+    isa     => Str,
+    default => '',
+);
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 
